2ad6f8a59930f1cc13d55054af6f65f77b152fe1
Markf1994 <markfenaroli@gmail.com>
2025-06-18 13:47:39 +0200

    Fix modello User

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,8 @@
-from pydantic import BaseModel
-class UserCreate(BaseModel):
-    email: str
-    password: str
-class UserResponse(BaseModel):
-    id: str
-    email: str
-    class Config:
-        orm_mode = True
+from sqlalchemy import Column, String
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
