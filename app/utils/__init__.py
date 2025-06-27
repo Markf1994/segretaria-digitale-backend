22a7636d15a67c8ee0e48cbcb86f2a706e8f11b3
Markf1994 <markfenaroli@gmail.com>
2025-06-27 23:44:27 +0200

    Remove unused email utility

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,3 @@
 """Utility helpers for the application."""
 
-# Re-export the :func:`send_email` helper so modules can simply do:
-# ``from app.utils import send_email``.
-
-from .email import send_email
+# Currently empty; add helper functions here as needed.
