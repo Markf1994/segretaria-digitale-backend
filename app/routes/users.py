68325f4d1187a53355365fd1b81cd266d086ffe7
Markf1994 <markfenaroli@gmail.com>
2025-06-27 13:14:40 +0200

    Check duplicate emails when creating users

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.schemas.user import UserCreate, UserResponse
@@ -13,5 +13,9 @@ def get_db():
         db.close()
 @router.post("/", response_model=UserResponse)
 def create_user_route(user_data: UserCreate, db: Session = Depends(get_db)):
+    existing_user = user.get_user_by_email(db, user_data.email)
+    if existing_user:
+        raise HTTPException(status_code=409, detail="Email already registered")
+
     db_user = user.create_user(db, user_data.email, user_data.password)
     return db_user
