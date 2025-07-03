2f7c34f3850833ba5742515f8611151c64aec61f
Markf1994 <markfenaroli@gmail.com>
2025-07-03 21:48:50 +0200

    Enable SQLite foreign keys

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -21,6 +21,13 @@ engine = create_engine(
     DATABASE_URL,
     connect_args=connect_args
 )
+
+if url.drivername.startswith("sqlite"):
+    @event.listens_for(engine, "connect")
+    def _enable_foreign_keys(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 # Esporta gli argomenti di connessione per i test
 CONNECT_ARGS = connect_args
