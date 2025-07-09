d51fa6064cbbd3f26203801878e71c8c5af8cb12
Markf1994 <markfenaroli@gmail.com>
2025-07-09 18:22:26 +0200

    Remove unused SHIFT_CAL_ID variable

--- app/services/gcal.py
+++ app/services/gcal.py
@@ -56,7 +56,6 @@ def get_client():
 
 
 # ------------------------------------------------------------------- calendar ID
-SHIFT_CAL_ID = settings.G_SHIFT_CAL_ID  # calendario "Turni di Servizio"
 
 
 # ------------------------------------------------------------------- utilità
