648e26e42771d644fa841d30d0fb9c21048558eb
Markf1994 <markfenaroli@gmail.com>
2025-07-09 09:55:00 +0200

    Add shift calendar ID default for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,4 @@ import os
 # Ensure a default secret key is available for tests
 os.environ.setdefault("SECRET_KEY", "test-secret-key")
 os.environ.setdefault("GOOGLE_CLIENT_ID", "test-client")
+os.environ.setdefault("G_SHIFT_CAL_ID", "test-shift-cal")
